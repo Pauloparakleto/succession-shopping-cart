26ed2a513209fcfe7fbff6bcde328f9b546e8780
Felipe <alparakleto@gmail.com>
2022-01-06 12:33:58 -0300

    test(shopping_cart_spec.rb): set and get initial attributes

--- spec/shopping_cart_spec.rb
+++ spec/shopping_cart_spec.rb
@@ -5,15 +5,15 @@ RSpec.describe ShoppingCart::Item do
     expect(ShoppingCart::VERSION).not_to be nil
   end
 
- context 'when initialize' do
-  let!(:item_ids){[1, 2, 2, 3]}
+  context "when initialize" do
+    let!(:item_ids) { [1, 2, 2, 3] }
 
-  it 'has items ids' do
-    expect(described_class.new(item_ids).product_ids).to eq(item_ids)
-  end 
+    it "has items ids" do
+      expect(described_class.new(item_ids).product_ids).to eq(item_ids)
+    end
 
-  it 'has unique item ids' do
-    expect(described_class.new(item_ids).product_unique_ids).to eq([1, 2, 3])
+    it "has unique item ids" do
+      expect(described_class.new(item_ids).product_unique_ids).to eq([1, 2, 3])
+    end
   end
- end
 end
